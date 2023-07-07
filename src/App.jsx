43298984dc45162840f498bd4ec2cfec66aab5fa
Ducphan2k <119691110+Ducphan2k@users.noreply.github.com>
2023-07-07 20:40:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Alert from "./components/Alert";
+
+const App = () => {
+  return (
+    <div>
+      <Alert />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Alert.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+
+const Alert = () => {
+  return (
+    <div className="alert alert-warning" role="alert">
+      Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+    </div>
+  );
+};
+
+
+
+export default Alert;
+
